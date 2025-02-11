4898eed1b65569f0ec4a000a80c552e13a2d7909
fiamabrenda <fiamabrenda94@gmail.com>
2025-02-11 16:59:18 -0300

    Utilizando pageable

--- src/main/java/com/senai/ecommerce/entities/Produto.java
+++ src/main/java/com/senai/ecommerce/entities/Produto.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,8 +23,12 @@ public class Produto {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	
+	@Column(columnDefinition = "TEXT")
 	private String descricao;
 	private Double preco;
+	
+	@Column(columnDefinition = "TEXT")
 	private String imgUrl;
 
 	@ManyToMany
